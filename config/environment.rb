fb5afeb72d3648445dab766e339c973dd75b4309
tbostelmann User <tbostelmann@ubuntu.ubuntu-domain>
2009-04-11 16:00:54 -0700

    added gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -24,11 +24,16 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "hpricot"
-  config.gem "haml"
-  config.gem "htmlentities"
-  config.gem "money"
-  config.gem "RedCloth"
+  config.gem "hpricot", :version => '0.8.1'
+  config.gem "haml", :version => '2.0.9'
+  config.gem "htmlentities", :version => '4.0.0'
+  config.gem "money", :version => '2.1.3'
+  config.gem "RedCloth", :version => '4.1.9'
+  config.gem "rake", :version => '0.8.4'
+  config.gem "mongrel", :version => '1.1.5'
+  config.gem "activemerchant", :lib => "active_merchant", :version => "1.4.1"
+  config.gem "populator", :version => "0.2.5"
+  config.gem "faker", :version => "0.3.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
