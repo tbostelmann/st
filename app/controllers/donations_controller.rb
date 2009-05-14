601576cd8112899d22042ba8be224f20e2243555
Doug Scott <duglessxott@gmail.com>
2009-05-14 10:12:54 -0700

    Fix flow for new donations with validated notification email

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -11,13 +11,8 @@ class DonationsController < BaseController
     @donation = new_default_donation(user, @adc)
 
     respond_to do |format|
-      if @donation.valid?
-        format.html # new.html.erb
-        format.xml  { render :xml => @donation }
-      else
-        format.html { render :action => "new", :saver_id => @saver_id }
-        format.xml  { render :xml => @donation.errors, :status => :unprocessable_entity }
-      end
+      format.html # new.html.erb
+      format.xml  { render :xml => @donation }
     end
   end
 
