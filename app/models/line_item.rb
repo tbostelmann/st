abf1eccc63a47bdf065db20139a21401dcfde019
Doug Scott <duglessxott@gmail.com>
2009-05-29 13:50:58 -0700

    Fix typo that fails app startup

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -23,7 +23,7 @@ class LineItem < ActiveRecord::Base
 
   composed_of :amount, :class_name => "Money", :mapping => [%w(cents cents)], :converter => Proc.new { |value| value.to_money }
 
-  validate_presence_of :financial_transactions
+  validates_presence_of :financial_transactions
   
   def status
     ft = financial_transactions.find(:first, :order => {:created_at => :desc}).status
