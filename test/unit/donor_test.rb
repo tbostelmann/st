809a5487546a3a4f3cb688e1898ade6adabfc031
Doug Scott <duglessxott@gmail.com>
2009-06-01 22:21:47 -0700

    Final donor unit test (just test additional ST requirements on Donor (CE already as their own User unit test))

--- test/unit/donor_test.rb
+++ test/unit/donor_test.rb
@@ -3,44 +3,28 @@ require File.dirname(__FILE__) + '/../test_helper'
 class DonorTest < ActiveSupport::TestCase
   
   def setup
-    @good_donor_properties = {
+    @properties_of_valid_cd_user = {
       :login => "foobar",
       :email => "a@b.com",
-      :email_confirmation => "a@b.com",
       :password => "foo2thebar",
       :password_confirmation => "foo2thebar",
       :birthday => 21.years.ago
       }
-    @bad_donor_properties = @good_donor_properties.dup
-    @bad_donor_properties.delete(:login)
-  end
-  
-  test "complete donor properties are good" do
-    donor = Donor.new(@good_donor_properties)
-    isValid = donor.valid?
-    donor.errors.each {|e, m| puts "Error: #{e} #{m}"}
-    assert isValid
-  end
-  
-  test "incomplete donor properties are bad" do
-    donor = Donor.new(@bad_donor_properties)
-    assert !donor.valid?
-  end
-  
-  test "Donor must have a login" do
-    assert false
-  end
-  
-  test "Donor login must be greater than six characters in length" do
-    assert false
-  end
-  
-  test "Donor must have an email" do
-    assert false
   end
   
   test "Donor email must be confirmed" do
-    assert false
+    donor = Donor.new(@properties_of_valid_cd_user)
+    assert !donor.valid?
+    assert donor.errors.on(:email)
+    
+    @properties_of_valid_cd_user[:email_confirmation] = "foo@bar.com"
+    donor = Donor.new(@properties_of_valid_cd_user)
+    assert !donor.valid?
+    assert donor.errors.on(:email)
+    
+    @properties_of_valid_cd_user[:email_confirmation] = "a@b.com"
+    donor = Donor.new(@properties_of_valid_cd_user)
+    assert donor.valid?
   end
   
   test "get the list of donations_given" do
