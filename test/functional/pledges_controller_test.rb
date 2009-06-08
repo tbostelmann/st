c0fc142e7106053d9859a2a05e4c11bb106f3182
tbostelmann User <tbostelmann@ubuntu.ubuntu-domain>
2009-06-08 15:28:44 -0700

    fixed tests

--- test/functional/pledges_controller_test.rb
+++ test/functional/pledges_controller_test.rb
@@ -10,77 +10,7 @@ class PledgesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "create valid donation and register valid user should render 'create' template" do
-    saver = users(:saver)
-    stOrg = Organization.find_savetogether_org
-
-    donor = Donor.find_by_login('testlogin')
-    assert donor.nil?
-
-    post :create, {
-        :saver_id => saver.id,
-        :pledge => { :donation_attributes => pledge_params(saver) },
-        :donor => {
-          :login => "testlogin@example.com",
-          :first_name => "Tess",
-          :last_name => "Loggin",
-          :login_confirmation => "testlogin@example.com",
-          :password => "password",
-          :password_confirmation => "password"} }
-
-    assert_template 'create'
-    assert_response :success
-
-    donor = Donor.find_by_login('testlogin@example.com')
-    assert !donor.nil?
-
-    # Use donor to find pledge and assert they're the same
-    assert !donor.pledges.empty?
-    d_pledge = donor.pledges[0]
-
-    test_pledge_no_fees(d_pledge)
-
-    # Reload pledge and assert it's values
-    pledge = Pledge.find(d_pledge.id)
-    assert !pledge.nil?
-    d_pledge.id == pledge.id
-
-    test_pledge_no_fees(pledge)
-  end
-
-  test "create valid donation and login valid user should render 'create' template" do
-    saver = users(:saver)
-    stOrg = Organization.find_savetogether_org
-    donor = users(:donor4)
-
-    post :create, {
-        :commit => :log_in.l,
-        :saver_id => saver.id,
-        :pledge => { :donation_attributes => pledge_params(saver) },
-        :login => donor.login,
-        :password => "test"}
-
-    assert_template 'create'
-    assert_response :success
-
-    donor = Donor.find(donor.id)
-    assert !donor.nil?
-
-    # Use donor to find pledge and assert they're the same
-    assert !donor.pledges.empty?
-    d_pledge = donor.pledges[0]
-
-    test_pledge_no_fees(d_pledge)
-
-    # Reload pledge and assert it's values
-    pledge = Pledge.find(d_pledge.id)
-    assert !pledge.nil?
-    d_pledge.id == pledge.id
-
-    test_pledge_no_fees(pledge)
-  end
-
-  test "create valid donation with logged in user should render 'create' template" do
+  test "create valid donation with logged in user should render create template" do
     saver = users(:saver)
     stOrg = Organization.find_savetogether_org
     donor = users(:donor4)
