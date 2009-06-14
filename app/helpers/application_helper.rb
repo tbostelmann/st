ea6d81677e38f4ef59b298cfe3853217951fcdcf
tbostelmann User <tbostelmann@ubuntu.ubuntu-domain>
2009-06-14 09:59:07 -0700

    unfinished report work - checking in for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
     end
     super(record, column)
   end
+
+  def build_date_from_params(field_name, params)
+    Date.new(params["#{field_name.to_s}(1i)"].to_i,
+         params["#{field_name.to_s}(2i)"].to_i,
+         params["#{field_name.to_s}(3i)"].to_i)
+  end  
 end
