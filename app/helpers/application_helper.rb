815d4962f8ff70ea918c24cac7d696026da883a3
Doug Scott <duglessxott@gmail.com>
2009-07-07 19:32:20 -0700

    Half of issue 226: Make menu entry for SaveTogether ask stick to user selection, update percentage reported depending on changing total of pledge if user modifies any of the rest of their donations or pledge.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,20 +21,41 @@ module ApplicationHelper
   def select_savetogether_amounts_cents_values
     factors = [0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5]
     opts = []
-    ctotal = 0
+    total_cents = 0
+    st_ask_cents = nil
     storg_id = Organization.find_savetogether_org.id
     find_pledge.donations.each do |d|
       unless d.to_user_id == storg_id
-        ctotal = ctotal + d.cents
+        total_cents = total_cents + d.cents
+      else
+        st_ask_cents = d.cents
       end  
     end  
 
     factors.each_with_index do |f, i|
-      camount = ctotal.to_f * f
-      percent = f * 100.to_f
-      amount = Money.new(camount.to_i)
-      opts << [:donation_amount_to_st.l(:amount => amount.format(:html), :percent => percent), camount.to_i]
+      opts << savetogether_donation_menu_entry(factored_cents(total_cents, f), percent_from_factor(f))
     end
+    
+    if (st_ask_cents) then
+      opts << savetogether_donation_menu_entry(st_ask_cents, percent_from_factor(st_ask_cents.to_f/total_cents.to_f))
+      opts.uniq!
+      opts = opts.sort{|this, that| this[1] <=> that[1]}
+    end
+        
     return opts
   end
+
+private
+  def savetogether_donation_menu_entry(cents, percent)
+    [:donation_amount_to_st.l(:amount => Money.new(cents).format(:html), :percent => "%-.1f" % percent), cents]
+  end
+
+  def factored_cents(cents, factor)
+    (cents.to_f * factor).to_i
+  end
+  
+  def percent_from_factor(factor)
+    factor * 100.to_f
+  end
+
 end
