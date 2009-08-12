901052ded904b1ae8867ee44aa82c2fe4982dabe
Doug Scott <duglessxott@gmail.com>
2009-08-12 12:05:10 -0700

    Issue 318: Fix up landing page after Do More donor survey completed - progress - tbd fix up unit tests

--- app/controllers/donor_surveys_controller.rb
+++ app/controllers/donor_surveys_controller.rb
@@ -1,19 +1,29 @@
 class DonorSurveysController < BaseController
-  def new
+
+  def show
     if (params[:thank_you_for_pledge])
       flash[:thank_you_for_pledge] = true
     end
+
     flash.keep(:thank_you_for_pledge)
-    if current_user
+
+    # If completed_survey, we redirected after successfully recording survey - redisplay
+    # Otherwise suggest survery answers based on current user if logged in
+    # Otherwise show empty form
+
+    if params[:completed_survey]
+      @donor_survey = DonorSurvey.find_by_id(params[:completed_survey])
+    elsif current_user
       @donor_survey = DonorSurvey.new(
               :donor_id => current_user.id,
               :first_name => current_user.first_name,
               :last_name => current_user.last_name)
-    else
-      @donor_survey = DonorSurvey.new
     end
 
-    render :new
+    # Default: start a new survey
+    @donor_survey = DonorSurvey.new unless @donor_survey
+
+    render :show
   end
 
   def create
@@ -22,9 +32,8 @@ class DonorSurveysController < BaseController
 
     if @donor_survey.save
       flash[:thank_you_for_donor_survey] = true
-      render :create
-    else
-      render :new
     end
+
+    redirect_to :controller => :donor_surveys, :action => :show, :completed_survey => @donor_survey
   end
 end
