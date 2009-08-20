359907efd274e5db29f4afe67be4d5cc1cd33d33
Doug Scott <duglessxott@gmail.com>
2009-08-20 15:27:38 -0700

    Issue 377: Fix login-after redirect issues introduced by redirect addition for post-login on Invite form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -42,13 +42,26 @@ class SessionsController < BaseController
   # cases, when to login, an appliation type that ST is. In this case you want to redirect
   # back to where the user actually WAS.
   def store_current_location
-    logger.debug{"Requested resource: #{request.path}"}
-    logger.debug{"Referrer:           #{request.referrer}"}
-    logger.debug{"Current return_to:  #{session[:return_to]}"}
-    # Leave return_to state set to last return_to if we're at the login page
-    # This means we likely came from somewhere in the app redirecting to login.
-    # So we want to go back to *that* referrer
-    session[:return_to] = request.referrer unless request.referrer =~ /\/login$/
+    logger.debug{"Requested resource : #{request.path}"}
+    logger.debug{"Referrer           : #{request.referrer}"}
+    logger.debug{"Current return_to  : #{session[:return_to]}"}
+    logger.debug{"Updated return_to  : #{updated_return_to}"}
+
+    session[:return_to] = updated_return_to
+  end
+  
+  # Leave return_to state set to last return_to if we're at the login page
+  # This means we likely came from somewhere in the app redirecting to login.
+  # So we want to go back to *that* referrer
+  def updated_return_to
+    case request.referrer
+      when /\/login$/, /\/sessions$/, /\/sessions\/new$/
+        session[:return_to] # preserve current return to
+      when /\/signup$/
+        nil                 # Forces CE redirector to go to profile
+      else
+        request.referrer    # return to the referring page
+    end
   end
 
 end
