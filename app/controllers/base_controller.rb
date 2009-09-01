0519b90abd17f0a464a4e3e6291e03a23b323c79
Doug Scott <duglessxott@gmail.com>
2009-09-01 16:57:35 -0700

    Progress on new home page

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,6 @@
 class BaseController < ApplicationController
   def site_index
-    @savers = Saver.featured_savers
+    @savers = Saver.featured_savers(3)
     # DPIRONE: note this does a lot of extra work to get CE meta data that we presently don't use,
     # so commenting out to save CPU cycles ...
     # respond_to do |format|
