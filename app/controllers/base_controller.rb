00c353266fcd71b7092b8878fe7f6ebae1e642ff
Doug Scott <duglessxott@gmail.com>
2009-09-04 11:27:45 -0700

    Move reference of static data to controllers for one-stop replacement later. Organize views to have no knowledge of it.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,8 @@
 class BaseController < ApplicationController
   def site_index
-    @savers = Saver.featured_savers(3)
+    @successful_saver = RANDOM_SUCCESSFUL_SAVER # TODO (dscott) replace this with a new Saver.random_successful_saver call
+    @featured_savers  = Saver.featured_savers(3)
+
     # DPIRONE: note this does a lot of extra work to get CE meta data that we presently don't use,
     # so commenting out to save CPU cycles ...
     # respond_to do |format|
