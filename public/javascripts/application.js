b4aad8be487632cb902a531038a792c74d507a97
tbostelmann User <tbostelmann@ubuntu.ubuntu-domain>
2009-09-23 09:24:21 -0700

    adding finishing touches to take-action page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,25 +5,39 @@ var i=0;
 var l;
 var t;
 var successStories;
+
 var i2=0;
 var l2;
 var t2;
 var featuredDonors;
+
 var i3 = 0;
 var t3;
 var l3;
 var partnerList;
 
+var i4 = 0;
+var t4;
+var l4;
+var quoteList;
+
 window.onload=function iterate_fade_inout() {
     successStories = document.getElementsByClassName('success-story');
     l = successStories.length;
+
     featuredDonors = document.getElementsByClassName('featured-donor');
     l2 = featuredDonors.length;
+
     partnerList = document.getElementsByClassName('partner-list');
     l3 = partnerList.length;
-    fade_inout();
-    fd_fade_inout();
-    pl_fade_inout();
+
+    quoteList = document.getElementsByClassName('take-action-quote');
+    l4 = quoteList.length;
+
+    if (l > 0) {fade_inout();}
+    if (l2 > 0) {fd_fade_inout();}
+    if (l3 > 0) {pl_fade_inout();}
+    if (l4 > 0) {quote_fade_inout();}
 }
 
 function fade_inout() {
@@ -51,4 +65,13 @@ function pl_fade_inout() {
     partnerList[(i3 % l3)].id = 'plFadeOn'
     new Effect.Appear('plFadeOn', {queue: {scope: 'two', position: 'end'}})
     t3 = setTimeout("pl_fade_inout()",10000);
+}
+
+function quote_fade_inout() {
+    new Effect.Fade('quoteFadeOn')
+    quoteList[(i4 % l4)].id = ''
+    i4++;
+    quoteList[(i4 % l4)].id = 'quoteFadeOn'
+    new Effect.Appear('quoteFadeOn', {queue: 'end'})
+    t4 = setTimeout("quote_fade_inout()",10000);
 }
