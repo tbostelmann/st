3055ee9a400d30ef90ee7cf13fb97cd950a79bdb
Tom Bostelmann <tbostelmann@tbostelmann-laptop.(none)>
2009-12-18 14:38:24 -0800

    reversed changes to a frozen gem

--- vendor/gems/acts_as_reportable-1.1.1/init.rb
+++ vendor/gems/acts_as_reportable-1.1.1/init.rb
@@ -1,3 +1,9 @@
 
-require File.join(File.dirname(__FILE__), 'lib', 'ruport', 'acts_as_reportable.rb')
+require_options = ["acts_as_reportable"]
+if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
+  require File.join(File.dirname(__FILE__), 'lib', require_lib)
+else
+  puts msg = "ERROR: Please update #{File.expand_path __FILE__} with the require path for linked RubyGem acts_as_reportable"
+  exit
+end
 
